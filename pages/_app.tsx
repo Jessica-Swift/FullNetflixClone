9e23faaa2a6df764e9bf8293675592af05481b71
Jessica Kim <jessica.kim@outlook.de>
2022-08-31 22:58:08 +0200

    finished user authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { AuthProvider } from "../hooks/useAuth";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // die ganze App (Component) ist im AuthProvider eingeschlossen, also im Login --> HOC
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
