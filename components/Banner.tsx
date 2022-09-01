54b68d5c0848a99fdef5479be6259348717622cf
Jessica Kim <jessica.kim@outlook.de>
2022-09-01 23:14:02 +0200

    building modal

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,6 +4,8 @@ import { baseUrl } from "../constants/movie";
 import { Movie } from "../typings";
 import { FaPlay } from "react-icons/fa";
 import { InformationCircleIcon } from "@heroicons/react/solid";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtoms";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -11,6 +13,8 @@ interface Props {
 
 function Banner({ netflixOriginals }: Props) {
   const [movie, setMovie] = useState<Movie | null>(null);
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
 
   useEffect(() => {
     setMovie(
@@ -32,7 +36,7 @@ function Banner({ netflixOriginals }: Props) {
       <h1 className="text-2xl md:text-4xl lg:text-7xl font-bold">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
- 
+
       <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
         {movie?.overview}
       </p>
@@ -42,7 +46,13 @@ function Banner({ netflixOriginals }: Props) {
           <FaPlay className="h-4 w-4  text-black md:h-7 md:w-7" />
           Play
         </button>
-        <button className="bannerButton bg-[gray]/70">
+        <button
+          className="bannerButton bg-[gray]/70"
+          onClick={() => {
+            setCurrentMovie(movie);
+            setShowModal(true);
+          }}
+        >
           More Info
           <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
         </button>
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import MuiModal from "@mui/material/Modal";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { modalState } from "../atoms/modalAtoms";
+import { XIcon } from "@heroicons/react/outline";
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
   const handleClose = () => {
-    setShowModal(false)
+    setShowModal(false);
   };
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
-      <>Modal</>
+      <>
+        <button
+          onClick={handleClose}
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+        >
+          <XIcon className="h-6 w-6"></XIcon>
+        </button>
+      </>
     </MuiModal>
   );
 }
